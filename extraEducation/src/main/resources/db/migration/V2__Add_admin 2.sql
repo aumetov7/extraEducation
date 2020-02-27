dfdadf263fdc5d49d4b2872ba5bbeac50b1ca163
Akbar <aumetov7@gmail.com>
2020-02-28 02:40:48 +0300

    small decorating changes
    user edit form changes

--- extraEducation/src/main/resources/db/migration/V2__Add_admin 2.sql
+++
@@ -1,5 +0,0 @@
-insert into usr (id, username, password, first_name, last_name, number, active)
-    values (0, 'admin', '$2y$08$4uW7lrMn3quP3p6CpMqjwuS/bdjHNJ8eE3We4rBURnbCiZ5JSirLG', 'admin', 'admin', '-', true);
-
-insert into user_role (user_id, roles)
-    values (0, 'USER'), (0, 'ADMIN');
